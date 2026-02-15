3bbac571161a676b51e91a2cb409a93ad56441e6
Rajukumar <rajuk631149@gmail.com>
2026-02-15 23:13:57 +0530

    Add initial project files for CI/CD practice including HTML, CSS, JS, and GitHub Actions workflow

---
+++ GitHub-Workflows/2nd-Basic-Project-Practice/Project-details.md
@@ -0,0 +1,8 @@
+# ci-cd-practice-project
+
+│
+├── index.html
+├── script.js
+└── .github
+    └── workflows
+        └── ci.yml
---
+++ GitHub-Workflows/2nd-Basic-Project-Practice/script.js
@@ -0,0 +1,4 @@
+console.log("working this is script.js file for 2nd basic project practice");
+console.log("CI/CD Practice Project");
+
+
